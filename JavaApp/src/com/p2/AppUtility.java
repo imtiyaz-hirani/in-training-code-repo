80305e36f524619f548f29bdb17aa4445a945b91
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-03-07 22:41:58 +0530

    static variable

--- JavaApp/src/com/p2/AppUtility.java
+++ JavaApp/src/com/p2/AppUtility.java
@@ -1,8 +1,11 @@
 package com.p2;
 
 public class AppUtility {
-	
+	private static  double num= 6; 
+	/* If you are using the variable inside static method, make it as static*/
 	public static String replaceCommaWithHiphen(String str) {
+		System.out.println(num);
 		return str.replace(",", "-");
 	}
 }
+//interest amount = p*n*r / 100 
